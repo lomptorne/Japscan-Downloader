d2c6b6b1328a5ff58768e05c2c18e491dd75dd72
lamptorne <lamptorne@debian>
2020-05-15 15:40:21 +0200

    bug correction, adding a gui

--- japscan_downloader.py
+++ japscan_downloader.py
@@ -1,295 +1,212 @@
-import os 
 import sys
-import requests 
-import psutil
-import time
-import csv 
-import inquirer
-import pandas as pd 
-import string
+import os
+import time 
+import traceback
 
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from helpers import search, chapters, urlMaker, killer, downloader, saver
 from bs4 import *
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
 from browsermobproxy import Server
 from tqdm import tqdm
+class WorkerSignals(QObject):
 
-class japscan_downloader:
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
+
+class Worker(QRunnable):
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()    
+
+        # Add the callback to our kwargs
+        self.kwargs['progress_callback'] = self.signals.progress        
+
+    @pyqtSlot()
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)  # Return the result of the processing
+        finally:
+            self.signals.finished.emit()  # Done
+
+# Init the main windows
+class Windows(QWidget):
 
 	def __init__(self):
-		pass
 
-	def builder(self) :
+		super().__init__()
+		self.initUI()
 
-		# Set the basics variables 
-		base_url ="https://www.japscan.co"
-		chap_returns = []
-		self.urls_list = []
-		name_results = []
-		chap_results = []
-		url_n = {"name" : "url"}
-		url_c = {"chapter" : "url"}
-		no_result = True
-		search_results = []
 
-		while( len(search_results) < 1 ) : 
+	def initUI(self):
+		self.step = 0
+		# Set the labels elements
+		label_input = QLabel()
+		label_manga = QLabel()
+		label_chapter = QLabel()
+		label_progress = QLabel()
+		label_input.setText("Mangas Search :")
+		label_manga.setText("Mangas :")
+		label_chapter.setText("Chapters :")
 
-			# Get the input from the user and convert it into a lowercase string
-			search = input("Enter the manga title : ")
-			str(search)
-			search = search.lower()
-			search = search.replace(" ", "-")
-			
-			# Query the link csv file to check for results 
-			df = pd.read_csv('urls.csv')
-			df_tolist= df[df['0'].str.contains(search)]
-			search_results = df_tolist.values.tolist()
+		# Set the buttons elements
+		self.btn_download = QPushButton("Download")
+		btn_exit = QPushButton("Exit")
+		self.btn_search = QPushButton("Search")
 
-			if len(search_results) < 1 :
-				print("No result")
-
-		# add all the searchs results and excrat the name from the url 
-		for search_result in search_results :
-
-			name_raw = search_result[0]
-			name = name_raw.split("/")[2]
-			name = name.replace("-", " ")
-			name = string.capwords(name)
-			name_results.append(name)
-			url_n[name] = '{}'.format(search_result)
-
-		# Promt user to choose a manga
-		questions = [
-		  				inquirer.List('result',
-		                message="Choose a result",
-		                choices= name_results,
-		            ),
-		]
-		answer = inquirer.prompt(questions)
-		self.manga_name = answer["result"]
-		end_url = url_n["{}".format(self.manga_name)]
-		end_url = end_url.replace("[", "").replace("]","").replace("'", "")
-
-		# Add the manga to the url 
-		manga_url = base_url + end_url
+		# Set the lists elements
+		self.list_manga = QListWidget()
+		self.list_chapter = QListWidget()
 		
-		# Set the soup 
-		page = requests.get(manga_url)
-		soup = BeautifulSoup(page.content, 'html.parser')
+		# Set the searchbar
+		self.input_search = QLineEdit(self)
 
-		# Collect Urls from the correct element on the page 
-		for div in soup.find_all('div', attrs={'id': 'chapters_list'}):
+		# Set the progress bar
+		self.bar_progress = QProgressBar(self)
+		self.bar_progress.setGeometry(30, 40, 200, 25)
 
-			for a in div.find_all('a', href=True):
+		# Set the threadpool
+		self.threadpool = QThreadPool()
+		self.threadpool.setMaxThreadCount(1)
 
-					chap_returns.append(a['href'])
-
-			for chap_return in chap_returns :
-				name = chap_return.split("/")[3]
-				name = name.replace("-", " ")
-				name = string.capwords(name)
-				chap_results.append(name)
-				url_c[name] = '{}'.format(chap_return)
-
-		# Ask the user for the Chapter/Volume
-		try :
-			questions = [
-							inquirer.List('chapter',
-							message= "Choose a chapter",
-							choices= chap_results,
-							),
-			]
-			answer = inquirer.prompt(questions)
-			self.chapter_name = answer["chapter"]
-			chap_url = url_c["{}".format(answer["chapter"])]
-			chap_url = chap_url.replace("[", "").replace("]","").replace("'", "")
-
-		except :
-			print("Error chapter")
-			sys.exit()
-
-		# Make the chapter/volume url
-		basic_url = str(base_url + chap_url)
+		# Set the list layouts
+		box_list_m = QHBoxLayout()
+		box_list_m.setAlignment(Qt.AlignCenter)
+		box_list_v1 = QVBoxLayout()
+		box_list_v2 = QVBoxLayout()
+		box_list_v1.addWidget(label_manga)
+		box_list_v1.addWidget(self.list_manga)
+		box_list_v2.addWidget(label_chapter)
+		box_list_v2.addWidget(self.list_chapter)
+		box_list_m.addLayout(box_list_v1)
+		box_list_m.addLayout(box_list_v2)
 		
+		# Set the search box
+		box_search_m = QVBoxLayout()
+		box_search_h = QHBoxLayout()
+		box_search_h.addWidget(self.input_search)
+		box_search_h.addWidget(self.btn_search)
+		box_search_m.addWidget(label_input)
+		box_search_m.addLayout(box_search_h)
+
+		# Set the bottom buttons layout
+		box_button = QHBoxLayout()
+		box_button.addWidget(self.btn_download)
+		box_button.addWidget(btn_exit)
+
+		# Set the main windows box 
+		box_main = QVBoxLayout()
+		box_main.addLayout(box_search_m)
+		box_main.addLayout(box_list_m)
+		box_main.addWidget(self.bar_progress)
+		box_main.addLayout(box_button)
+
+		# Set the windows 
+		self.setLayout(box_main)
+		self.setGeometry(500, 300, 300, 200)
+		self.setWindowTitle('Japscan Downloader')
+		self.show()
+
+		#Set the buttons and their action
+		btn_exit.clicked.connect(self.close)
+		self.btn_search.clicked.connect(self.function_search)
+		self.list_manga.itemClicked.connect(self.function_chapter)
+		self.list_chapter.itemClicked.connect(self.builder)
+		self.btn_download.clicked.connect(self.launcher)
+		self.btn_download.setEnabled(False)
+
+
+
+	# When convert button is clicked call the convert function
+	def function_search(self):
+
+		self.list_manga.clear()
+		self.list_chapter.clear()
+		request_search = self.input_search.text()
+		self.search_results = search(request_search)
 		
-		# Update the soup 
-		page = requests.get(basic_url)
-		soup = BeautifulSoup(page.content, "html.parser")
+		if self.search_results != None :
+			for search_result in self.search_results :
+				self.list_manga.addItem(search_result)
+		else:
+			self.list_manga.addItem("No Results")
+
+	def function_chapter(self):
 		
-		# Get the last page of the chapter
-		last_page = soup.find('select', attrs={'id': 'pages'}).find_all('option')[-1].get_text()
+		self.list_chapter.clear()
+		self.name_manga = self.list_manga.currentItem().text()
+		self.url_manga = self.search_results["{}".format(self.name_manga)][0]
+		self.request_chapters = chapters(self.name_manga, self.url_manga)
 		
-		# Isolate the page number to an int
-		pages_number = [int(word) for word in last_page.split() if word.isdigit()][0]
-
-		# Build all the urls depending on the number of pages
-		for i in range(pages_number) :
-
-			if i == 0:
-
-				self.urls_list.append(str(basic_url + str(i) + ".html"))
-
-			at_page = str(i+ 1) 
-
-			self.urls_list.append(str(basic_url + at_page + ".html"))
+		for request_chapter in self.request_chapters :
+			self.list_chapter.addItem(request_chapter)
 		
-		# Return a list with all the chapter/volume urls
-		return 
-
-	# Kill all the process that browsermob leave behind...
-	def killer(self):
-
-		for proc in psutil.process_iter():
-			if proc.name() == "browsermob-proxy":
-				proc.kill()
-				
-		for proc in psutil.process_iter():
-			if proc.name() == "java":
-				proc.kill()
-
-	def worker(self):
-
-		# Set basic var for the function
-		self.urls_down = []
-		network_events = []
-		URLS=self.urls_list 
-		page_nbr =len(self.urls_list) -1
-		path = os.getcwd()
-		# Browsermob binaries location
-		browsermobproxy_location = "{}/browsermob/browsermob-proxy".format(path) 
+	def function_url(self, progress_callback):
 		
-		# Start browsermob server
-		print("Proxy init...")
-		server = Server(browsermobproxy_location)
-		server.start()
-		time.sleep(1)
-		proxy = server.create_proxy()
-		time.sleep(1)
-
-		# Set option for the webdriver, automation detection from japscan, certificate, and headless 
-		chrome_path = "{}/chromedriver".format(path)
-		chrome_options = webdriver.ChromeOptions()
-		chrome_options.add_experimental_option("useAutomationExtension", False)
-		chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-		chrome_options.set_capability("acceptInsecureCerts", True)        
-		chrome_options.add_argument("--log-level=3")
-		chrome_options.add_argument('--proxy-server=%s' % proxy.proxy)
-		chrome_options.add_argument("--disable-blink-features")
-		chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-		chrome_options.add_argument("--headless")  
-		caps = DesiredCapabilities.CHROME
-		driver = webdriver.Chrome(chrome_path, desired_capabilities=caps, options=chrome_options)
-		print("Driver init...")
-
-		# Do a while loop in case of timeout it happen sometimes 
-		while True:
-
-			print("Fetch :")
-			try:
-				# Initiate the driver with low consumption website
-				driver.set_page_load_timeout(30)
-				driver.get('http://perdu.com/')
-				
-				# if the page number is even scrap only even page, since we can scrap the current page and the next page it's shorter
-				if page_nbr % 2 == 0 :
-					
-					for URL in tqdm(URLS[::2]) :
-						network_events = []
-						proxy.new_har("urls")
-						driver.get(URL)
-						
-						# Get the page logs
-						entries = proxy.har['log']["entries"]
-						for entry in entries:
-						    if 'request' in entry.keys():
-						        network_events.append(entry['request']['url'])
-						
-						# Extract only the imges 
-						matches = [s for s in network_events if ".jpg" in s and  "japscan.co" in s or ".png" in s and "japscan.co" in s]
-						matches = [ x for x in matches if "bg." not in x ]
-						
-						# Add images Urls to a list
-						for match in matches :
-
-							self.urls_down.append(match)
-
-				# Same operation if page number is odd 
-				if page_nbr % 2 != 0 :
-						
-						for URL in tqdm(URLS[1::2]) :
-							network_events = []
-							proxy.new_har("urls")
-							driver.get(URL)
-
-							entries = proxy.har['log']["entries"]
-							for entry in entries:
-							    if 'request' in entry.keys():
-							        network_events.append(entry['request']['url'])
-							
-							matches = [s for s in network_events if ".jpg" in s and  "japscan.co" in s or ".png" in s and "japscan.co" in s]
-							matches = [ x for x in matches if "bg." not in x ]
-
-							for match in matches :
-
-								self.urls_down.append(match)
-
-				break
-
-			except TimeoutException as ex:
-				print("Timeout, retry" + str(ex))
-				driver.quit()
-				continue
-
-		# Remove duplicate		
-		self.urls_down = list(dict.fromkeys(self.urls_down))
-
-		# Stop the server and the driver
-		server.stop()
-		driver.quit()
+		self.name_chapter = self.list_chapter.currentItem().text()
+		self.url_chapter = self.request_chapters["{}".format(self.name_chapter)][0]
+		self.urls = urlMaker(self.url_manga, self.url_chapter)
+		self.btn_download.setEnabled(True)	
 		
-		# Return image url list 
-		return 
+	def function_downloader(self, progress_callback):
 
-	def downloader(self):
+		self.list_chapter.setEnabled(False)
+		self.list_manga.setEnabled(False)
+		self.btn_download.setEnabled(False)
+		self.btn_search.setEnabled(False)
 		
-		URLS = self.urls_down
-		path = os.getcwd()
-		count = 0
-		dir_name = path + "/Mangas/" + self.manga_name + "/" + self.chapter_name
+		self.bar_progress.setRange(0,0)
+
+		killer()
+		urls_list = downloader(self.urls)
+		killer()
+
+		saver(urls_list, self.name_manga, self.name_chapter)
+
+		self.bar_progress.setRange(0, 1)
+
+		self.btn_search.setEnabled(True)
+		self.btn_download.setEnabled(True)
+		self.list_chapter.setEnabled(True)
+		self.list_manga.setEnabled(True)
+
+	def builder(self):
+		worker = Worker(self.function_url)
 		
-		# Create the folder 
-		try :
-			os.makedirs(dir_name)
-			os.chdir(dir_name)
-		except :
-			print("Folder already exist")
-			os.chdir(dir_name)
-		print("Download :")
-
-		# Downloading all the fetched urls 		
-		for URL in tqdm(URLS) :
-			
-			count += 1
-			response = requests.get(URL)
-
-			try :
-
-				file = open("{}.png".format(count), "wb")			
-				file.write(response.content)
-				file.close()
-			except : 
-				print("File already exist")
+		self.threadpool.start(worker)
 		
-		os.chdir(path)
 
-		print("Done !")
+	def launcher(self):
+		worker = Worker(self.function_downloader)
+		self.threadpool.start(worker) 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-	run = japscan_downloader()
-	run.killer()
-	run.builder()
-	run.worker()
-	run.killer()
-	run.downloader()
+    app = QApplication([])
+    ex = Windows()
+    sys.exit(app.exec_())
+
